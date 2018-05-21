7df5cd655b8c03d100516fb547adce01f95c7e40
jfernandes2001 <32169001+jfernandes2001@users.noreply.github.com>
2018-05-21 12:12:55 +0100

    Add files via upload

--- exercicio28.java
+++ exercicio28.java
@@ -1,25 +1,40 @@
-
-package priority.queue;
+package ex28;
 import java.util.PriorityQueue;
-public class exercicio28 {
+import java.util.Scanner;
+public class ex28 {
 
-    public static void main(String[] args) {
-     // Create Priority Queue
-      PriorityQueue<Integer> pq1 = new PriorityQueue<Integer>();  
-      PriorityQueue<Integer> pq2 = new PriorityQueue<Integer>();  
-        
-   // Add numbers in the Queue
-   pq1.add(5);
-   pq1.add(2);
-   pq1.add(8);
-   pq1.add(1);
-   pq1.add(2);
-   pq1.add(11);
-   pql.remove(5);
-   System.out.println("Original Priority Queue: "+pq1);
-   System.out.println("The first element of the Queue: "+pq1.peek());
-   }    
-}      
-    
-    
+	public static void main(String[] args) {
+		Scanner LerS= new Scanner(System.in);
+		PriorityQueue <Integer> pq1 = new PriorityQueue <Integer>();
+	int variavel;
+	System.out.print("Qual é a primeira variavel que quer introduzir?");
+	variavel = LerS.nextInt();
+	pq1.add(variavel);
+	int variavel1;
+	System.out.print("Qual é a segunda variavel que quer introduzir?");
+	variavel1 = LerS.nextInt();
+	pq1.add(variavel1);
+	int variavel2;
+	System.out.print("Qual é a terceira variavel que introduzir?");
+	variavel2 = LerS.nextInt();
+	pq1.add(variavel2);
+	int variavel3;
+	System.out.print("Qual é a quarta variavel que quer introduzir?");
+	variavel3 = LerS.nextInt();
+	pq1.add(variavel3);
+	int variavel4;
+	System.out.print("Qual é a quinta variavel que quer introduzir?");
+	variavel4 = LerS.nextInt();
+	pq1.add(variavel4);
+	int variavel5;
+	System.out.print("Qual é a sexta variavel que quer introduzir?");
+	variavel5 = LerS.nextInt();
+	pq1.add(variavel5);
+	System.out.print ("Flia Original:" +pq1);
+	System.out.print("A variavel à cabeça:" + pq1.element());
+	
+	
 
+	}
+
+}
