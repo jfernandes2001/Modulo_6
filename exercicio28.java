d1963f70fb164cd3580aed0c20deac6b7c14a343
jfernandes2001 <32169001+jfernandes2001@users.noreply.github.com>
2018-05-21 11:43:04 +0100

    Add files via upload

---
+++ exercicio28.java
@@ -0,0 +1,25 @@
+
+package priority.queue;
+import java.util.PriorityQueue;
+public class exercicio28 {
+
+    public static void main(String[] args) {
+     // Create Priority Queue
+      PriorityQueue<Integer> pq1 = new PriorityQueue<Integer>();  
+      PriorityQueue<Integer> pq2 = new PriorityQueue<Integer>();  
+        
+   // Add numbers in the Queue
+   pq1.add(5);
+   pq1.add(2);
+   pq1.add(8);
+   pq1.add(1);
+   pq1.add(2);
+   pq1.add(11);
+   pql.remove(5);
+   System.out.println("Original Priority Queue: "+pq1);
+   System.out.println("The first element of the Queue: "+pq1.peek());
+   }    
+}      
+    
+    
+
